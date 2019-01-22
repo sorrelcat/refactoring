5ffcbe80131fbb8f02b36d0d74dd4a6335a93370
o.shest@gmail.com <o.shest@gmail.com>
2019-01-22 14:46:46 +0200

    Full test list

--- calculator.cc
+++ calculator.cc
@@ -7,35 +7,54 @@
 /*
  * Calculator by MK
 */
-// Not obvious what is funciton result
-void split(const std::string& str, std::vector<std::string>& container, char delim = ' ')
+std::vector<std::string> split(const std::string& str, char delim = ' ')
 {
+    std::vector<std::string> container;
     std::stringstream ss(str);
     std::string token;
     // Not obvious that get line will fetch elements splitted by space
     while (std::getline(ss, token, delim)) {
         container.push_back(token);
     }
+    return container;
 }
 
-double calculate( const std::string input )
-{
-    std::vector<std::string> container;
-    split ( input, container );
-    auto a = std::stod( container[0]);
-    auto b = std::stod( container[1]);
-    char c = container[2][0];
+struct Result {
     double result;
-    switch (c)
-    {
-    case '+': result = a + b;
-        break;
-    case '-': result = a - b;
-        break;
-    case '*': result = a * b;
-        break;
-    case '/': result = a / b;
-        break;
+    bool state;
+    Result(double d , bool b): result(d = 0), state(b = true) {}
+};
+
+
+Result calculate( const std::string input )
+{
+    Result t;
+    int a, b;
+    char op;
+    std::vector<std::string> container = split (input);
+    // cycle for string that can contain more than 3 elements
+    // catch errors: less than 3 elements, not 3+2*n elements
+    // catch errors: not double or not operation symbol
+
+    if(container.size())
+    for(auto it : container) {
+
+
+        auto b = std::stod(*it++);
+        auto c = container[2][0];
+        double result;
+        switch (c)
+        {
+        case '+': result = a + b;
+            break;
+        case '-': result = a - b;
+            break;
+        case '*': result = a * b;
+            break;
+        //catch error: division by zero
+        case '/': result = a / b;
+            break;
+        }
     }
     return result;
 }
--- calculator.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCULATOR
-#define CALCULATOR
-#include <string>
-
-double calculate(const std::string& eval);
-
-#endif // CALCULATOR
